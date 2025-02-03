629477dca30895207dcd731d043a703bb9bff7c8
mtoh <soulskate143@gmail.com>
2025-02-03 16:43:40 +0800

    8th

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
-import path from 'path';
-
 export default {
-  experimental: {
-    appDir: true,
-  },
-  webpack(config) {
-    // Add src to module resolution
-    config.resolve.modules.push(path.resolve('src'));
-    return config;
-  },
-};
+    experimental: {
+      appDir: true,  // Enable the app directory feature
+    },
+  };
+  
